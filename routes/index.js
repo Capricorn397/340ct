8a01c83efcba9ccc913b94136042cb5415e6841e
Alex Bandtock <alex.b_1994@live.co.uk>
2016-10-07 13:47:07 +0100

    Basic login salt returning

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,26 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('partials/login');
 });
+// INPUT: { "user": "test" }
+// OUTPUT: {  }
+router.get('/api/login/salt', function(req, res, next) {
+  const saltQuery = "SELECT salt FROM users WHERE username='" + req.body.user + "'";
+  const results = [];
+  pg.connect(connectionString, (err, client, done) => {
+    if (err) {
+      done();
+      console.log(err);
+      return res.status(500), json({success: false, data: err});
+    }
+    const query = client.query(saltQuery);
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
 
 module.exports = router;
