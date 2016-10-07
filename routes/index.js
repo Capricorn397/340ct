5f8e30a193ea55d9d0bf860c3bf7b008c7b8f1d2
Alex Bandtock <alex.b_1994@live.co.uk>
2016-10-07 14:24:14 +0100

    Swapped to connection pools

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
+var Pool = require('pg').Pool;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,21 +15,23 @@ router.get('/login', function(req, res, next) {
 router.get('/api/login/salt', function(req, res, next) {
   const saltQuery = "SELECT salt FROM users WHERE username='alex'";//req.body.user
   const results = [];
-  const connectionString = "postgres://coursework_rw:StealthyChef@164.132.195.20:5432/coursework";
-  pg.connect(connectionString, (err, client, done) => {
+  var pool = new Pool({
+    user: "coursework_rw",
+    password: "StealthyChef",
+    host: "164.132.195.20",
+    database: "coursework",
+    max: 10,
+    idleTimeoutMillis: 1000
+  });
+  pool.on('error', function(e, client) {
+
+  });
+  pool.query(saltQuery, function(err, result) {
     if (err) {
-      done();
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
-    const query = client.query(saltQuery);
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
+    return res.json({salt: result.rows[0].salt});
   });
 });
 
