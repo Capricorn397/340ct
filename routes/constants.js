e15e6a9d6153e495bd66d28b84914514501bec9b
Alex Bandtock <alex.b_1994@live.co.uk>
2016-10-11 17:31:40 +0100

    Added constant.js for easy management of login details

---
+++ routes/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dbInfo: {
+    user: "coursework_rw",
+    password: "StealthyChef",
+    host: "164.132.195.20",
+    database: "coursework",
+    max: 10,
+    idleTimeoutMillis: 1000
+  }
+};
