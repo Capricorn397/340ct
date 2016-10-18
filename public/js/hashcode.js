bdcbab9e7a26f0d46072a622168bb0ae1601c4eb
Capricorn397 <chrisjcarr4@hotmail.co.uk>
2016-10-18 17:05:06 +0100

    Login testing

--- public/js/hashcode.js
+++ public/js/hashcode.js
@@ -1,9 +1,9 @@
 function hashcode(hashCode) {
 	console.log(hashCode);
   var hash = 0, i, c, l;
-  if (this.length === 0) return hash;
-  for (i = 0, l = this.length; i < l; i++){
-    c = this.charCodeAt(i);
+  if (hashCode.length === 0) return hash;
+  for (i = 0, l = hashCode.length; i < l; i++){
+    c = hashCode.charCodeAt(i);
     hash = ((hash << 5) - hash) + c;
   }
   return hash;
