07cb72b1d67d186fe243b1130e731ecd1de8dac9
Alex Bandtock <alex.b_1994@live.co.uk>
2016-11-10 12:49:02 +0000

    Finished promisifying

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -1,37 +1,39 @@
-var Pool = require('pg').Pool;
-var logins = require('../constants');
+'use strict'
 
-// Initiate db connection pool
-var pool = new Pool(logins.dbInfo);
-pool.on('error', function(e, client) {
-  console.log(e);
-});
+const Pool = require('pg').Pool
+const logins = require('../constants')
 
-exports.token = function(token, callback) {
-	const query = `SELECT login_token.user_id, users.rights_level \
-									FROM login_token \
-									INNER JOIN users ON (users.user_id = login_token.user_id) \
-									WHERE token = '${token}' \
-									AND (expiry_time IS NULL OR expiry_time > NOW())`
-	pool.query(query, function(err, result) {
-		if (err) {
-			console.log(err);
-			cb(new Error('Could not validate token.'));
-		}
-		if (result.rows.length === 0) {
-			cb(new Error('Token invalid.'));
-		} else {
-			cb(null, result.rows[0].user_id, result.rows[0].rights_level);
-			refreshToken(token);
-		}
-	});
-}
+const pool = new Pool(logins.dbInfo)
+pool.on('error', function(e) {
+	console.log(e)
+})
 
-function refreshToken(token) {
+exports.token = (token) =>
+	new Promise((resolve, reject) => {
+		const query = `SELECT login_token.user_id, users.rights_level \
+										FROM login_token \
+										INNER JOIN users ON (users.user_id = login_token.user_id) \
+										WHERE token = '${token}' \
+										AND (expiry_time IS NULL OR expiry_time > NOW())`
+		pool.query(query, function(err, result) {
+			if (err) {
+				console.log(err)
+				reject('Could not validate token.')
+			}
+			if (result.rows.length === 0) {
+				reject('Token invalid.')
+			} else {
+				refreshToken(token)
+				resolve({ 'id': result.rows[0].user_id, 'rights': result.rows[0].rights_level })
+			}
+		})
+	})
+
+const refreshToken = (token) => {
 	const query = `UPDATE login_token SET expiry_time = NOW() + interval '15 minutes' WHERE token = ${token}`
-	pool.query(query, function(err, results) {
+	pool.query(query, function(err) {
 		if (err) {
-			console.log(err);
+			console.log(err)
 		}
-	});
+	})
 }
--- routes/modules/login.js
+++ routes/modules/login.js
@@ -1,89 +1,95 @@
-var Pool = require('pg').Pool;
-var logins = require('../constants');
+'use strict'
 
-// Initiate db connection pool
-var pool = new Pool(logins.dbInfo);
-pool.on('error', function(e, client) {
-  console.log(e);
-});
+const Pool = require('pg').Pool
+const logins = require('../constants')
+const crypto = require('crypto')
+
+const pool = new Pool(logins.dbInfo)
+pool.on('error', function(e) {
+	console.log(e)
+})
 
 /**
  * Returns the salt for a given user
+ * @param {string} user - The username of the user
+ * @return {string} - The salt associated with the user
+ * @author Alex
  */
-exports.salt = function(user, cb) {
-  const saltQuery = "SELECT salt FROM users WHERE username='" + user + "'";
-  pool.query(saltQuery, function(err, result) {
-    if (err) {
-      console.log(err);
-      cb({success: false, data: err}, true);
-    }
-    if (result.rows.length === 0) {
-      cb({success: false, data: "Invalid username provided."}, false);
-    } else {
-      cb({salt: result.rows[0].salt}, false);
-    }
-  });
-};
+exports.salt = (user) =>
+	new Promise((resolve, reject) => {
+	  const saltQuery = `SELECT salt FROM users WHERE username='${user}'`
+	  pool.query(saltQuery, function(err, result) {
+	    if (err) {
+	      console.log(err)
+	      reject(err)
+	    }
+	    if (result.rows.length === 0) {
+	      reject('Invalid username provided.')
+	    } else {
+		    resolve({salt: result.rows[0].salt})
+	    }
+	  })
+	})
 
 /**
  * Returns a generated token for a given user/password
  */
-exports.token = function(user, hashedPassword, cb) {
-  verifyUser(user, hashedPassword, function(valid, userId) {
-    if (valid) {
-      genToken(userId, function(error, token) {
-        if (error) {
-          cb({success: false, data: "Failed to generate token"}, true);
-        } else {
-          cb({"token": token}, false);
-        }
-      });
-    } else {
-      cb({success: false, data: "Invalid password."}, true);
-    }
-  });
-}
+exports.token = (user, hashedPassword) =>
+	new Promise((resolve, reject) => {
+		verifyUser(user, hashedPassword).then((userId) => {
+	    genToken(userId).then((token) => {
+	    	resolve({'token': token})
+	    })
+		}).catch(() => {
+			reject('Failed to generate token')
+		})
+	})
 
 /**
  * Verifies correct password for a user
  */
-function verifyUser(user, hashedPassword, cb) {
-  const verificationQuery = "SELECT user_id FROM users WHERE username='"
-                            + user + "' AND hashed_password = '"
-                            + hashedPassword + "'";
-  pool.query(verificationQuery, function(err, result) {
-    if (err) {
-      console.log(err);
-      cb(false);
-    }
-    if (result.rows.length === 0) {
-      cb(false);
-    } else {
-      cb(true, result.rows[0].user_id);
-    }
-  });
-}
+const verifyUser = (user, hashedPassword) =>
+	new Promise((resolve, reject) => {
+	  const verificationQuery = `SELECT user_id FROM users \
+																WHERE username='${user}' \
+																AND hashed_password='${hashedPassword}'`
+	  pool.query(verificationQuery, function(err, result) {
+	    if (err) {
+	      console.log(err)
+	      reject(err)
+	    }
+	    if (result.rows.length === 0) {
+	      reject(false)
+	    } else {
+	      resolve(result.rows[0].user_id)
+	    }
+	  })
+	})
 
 /**
  * Generates a login token
  */
-function genToken(userId, cb) {
-  require('crypto').randomBytes(48, function(err, buffer) {
-    var token = buffer.toString('hex');
-    const tokenQuery = "INSERT INTO login_token (user_id, token, expiry_time) \
-                        VALUES (" + userId + ", '" + token +
-                        "', NOW() + interval \'15 minutes\') RETURNING token";
-    console.log(tokenQuery);
-    pool.query(tokenQuery, function(err, result) {
-      if (err) {
-        console.log(err);
-        cb(true);
-      }
-      if (result.rows.length === 0) {
-        cb(true);
-      } else {
-        cb(false, result.rows[0].token);
-      }
-    });
-  });
-}
+const genToken = (userId) =>
+	new Promise((resolve, reject) => {
+		const tokenLength = 48
+		crypto.randomBytes(tokenLength, function(err, buffer) {
+			if (err) {
+				reject(err)
+			}
+	    const token = buffer.toString('hex')
+	    const tokenQuery = `INSERT INTO login_token (user_id, token, expiry_time) \
+	                        VALUES ("${userId}, '${token}', NOW() + interval '15 minutes') \
+													RETURNING token`
+	    pool.query(tokenQuery, function(err, result) {
+	      if (err) {
+	        console.log(err)
+	        reject(err)
+	      }
+	      if (result.rows.length === 0) {
+	        reject('Error inserting row.')
+	      } else {
+	        resolve(false, result.rows[0].token)
+	      }
+	    })
+	  })
+	})
